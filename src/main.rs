aa659bd8c26d5c6b68976d3d985d492e7e63dab1
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2015-09-06 00:01:07 -0700

    switch to using a config file
    fixes #4

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 extern crate irc;
 extern crate rand;
 
-use std::default::Default;
+use std::env;
 use irc::client::prelude::*;
 use rand::{thread_rng, sample};
 
 fn main() {
     let mut rng = thread_rng();
     let choices = ["heads!", "tails!"];
-    let config = Config {
-        nickname: Some(format!("pdxferris")),
-        alt_nicks: Some(vec![format!("pdxrust-bot"), format!("pdx-rust-bot")]),
-        server: Some(format!("irc.freenode.net")),
-        channels: Some(vec![format!("#pdxbots"),
-                            format!("#pdxrust")]),
-        .. Default::default()
-    };
-    let server = IrcServer::from_config(config).unwrap();
+    // let config = Config {
+    //     nickname: Some(format!("pdxferris")),
+    //     alt_nicks: Some(vec![format!("pdxrust-bot"), format!("pdx-rust-bot")]),
+    //     server: Some(format!("irc.freenode.net")),
+    //     channels: Some(vec![format!("#pdxbots"),
+    //                         format!("#pdxrust")]),
+    //     .. Default::default()
+    // };
+    // let server = IrcServer::from_config(config).unwrap();
+    let server = IrcServer::new("config.json").unwrap();
     server.identify().unwrap();
     for message in server.iter() {
         let message = message.unwrap(); // We'll just panic if there's an error.
@@ -36,4 +37,9 @@ fn main() {
             }
         }
     }
+}
+
+#[test]
+fn test_main() {
+    main()
 }
